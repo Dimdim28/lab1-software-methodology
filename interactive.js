7a72d9760a019d723e0b8be46cd73c3773d8d0b6
Dimdim28 <wwwdimanes1@gmail.com>
2023-02-22 11:58:55 +0200

    Added checking for input values
    a,b,c now have validation

--- interactive.js
+++ interactive.js
@@ -4,15 +4,34 @@ const startInteractiveMode = () => {
   const questions = ["a = ", "b = ", "c = "];
   const params = [];
 
+  const checkNumber = (data) => {
+    return (
+      data.toString().trim() === parseInt(data.toString()).toString().trim() ||
+      data.toString().trim() === parseFloat(data.toString()).toString().trim()
+    );
+  };
+
   process.stdin.on("data", (data) => {
     if (params.length < questions.length) {
       const parsedData = parseFloat(data.toString());
-      params.push(parsedData);
-      if (params.length < questions.length) {
+      if (params.length === 0 && parsedData === 0) {
+        console.log("Error. a cannot be 0");
+        process.stdout.write(questions[0]);
+      } else if (!checkNumber(data)) {
+        console.log(
+          `Error. Expected a valid real number, got ${data
+            .toString()
+            .trim()} instead`
+        );
         process.stdout.write(questions[params.length]);
       } else {
-        console.log(quadraticEquationSolver(...params));
-        process.exit();
+        params.push(parsedData);
+        if (params.length < questions.length) {
+          process.stdout.write(questions[params.length]);
+        } else {
+          console.log(quadraticEquationSolver(...params));
+          process.exit();
+        }
       }
     }
   });
